9441fa5064aaf34032a7e0c2f5c74e41d1d6f966
swap357 <swapnilpatel357@gmail.com>
2025-01-31 21:37:23 +0530

    add zlib.dll to expected wheel imports validation

--- buildscripts/github/validate_win64_wheel.py
+++ buildscripts/github/validate_win64_wheel.py
@@ -29,5 +29,6 @@ for path in pathlib.Path('.').rglob("**/*.dll"):
         'api-ms-win-crt-string-l1-1-0.dll',
         'api-ms-win-crt-time-l1-1-0.dll',
         'api-ms-win-crt-utility-l1-1-0.dll',
+        'zlib.dll',
     }
     assert imports == expected_imports, f"Unexpected imports: {imports}"
